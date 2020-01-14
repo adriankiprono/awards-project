3ebb73adccf8d14f3763ac8b2b6f49dc175b1f76
adriankiprono <tuimuradrian6@gmail.com>
2020-01-14 15:28:44 +0300

    add search form

--- website/views.py
+++ website/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .models import *
 
 # Create your views here.
 
 def home(request):
+    awards = Award.objects.all()
     
-    return render(request,'home.html')
+    return render(request,'home.html',{"awards":awards})
