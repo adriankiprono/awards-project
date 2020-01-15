bf610999df82ca295795ec3803ebccb1884f9879
adriankiprono <tuimuradrian6@gmail.com>
2020-01-15 15:56:53 +0300

    add profile

--- website/models.py
+++ website/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 from tinymce.models import HTMLField
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
 class Award(models.Model):
@@ -22,6 +24,20 @@ class Profile(models.Model):
     name = models.CharField(max_length=40)
     bio = models.TextField(max_length= 100)
     profile_pic= models.ImageField(upload_to= 'avatar/',default='image2')
+
+    @receiver(post_save,sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.created(user=instance)
+
+    @receiver(post_save,sender=User)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.profile.save()
+    
+    @classmethod
+    def all_profiles(cls):
+        profiles = cls.objects.all()
+        return profiles
 
     def __str__(self):
         return self.name
